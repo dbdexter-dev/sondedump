615c047f2bda13636ae54655bd65d681513e00c3
dbdexter-dev <dbdexter.dev@gmail.com>
2022-07-07 15:56:04 +0200

    Fixed some compilation warnings

--- cmake/EmbedBlob.cmake
+++ cmake/EmbedBlob.cmake
@@ -9,5 +9,5 @@ string(REGEX REPLACE "\\.| |-" "_" SYMBOL_NAME ${SRC_FILE})
 file(READ ${IN_FILE} FILEDATA HEX)
 string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," FILEDATA ${FILEDATA})
 file(WRITE ${OUT_FILE} "const unsigned char _binary_${SYMBOL_NAME}[] = {${FILEDATA}};\n")
-file(APPEND ${OUT_FILE} "const unsigned long _binary_${SYMBOL_NAME}_size = sizeof(_binary_${SYMBOL_NAME});")
+file(APPEND ${OUT_FILE} "const unsigned long _binary_${SYMBOL_NAME}_size = sizeof(_binary_${SYMBOL_NAME});\n")
 
