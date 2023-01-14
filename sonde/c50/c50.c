1e8f0dd8b6714f5b1835c193d4f8703d4f8c6ad6
dbdexter-dev <dbdexter.dev@gmail.com>
2023-01-14 17:27:22 +0100

    Added some field parsing

--- sonde/c50/c50.c
+++ sonde/c50/c50.c
@@ -1,14 +1,18 @@
 #include <include/c50.h>
 #include <string.h>
+#include "bitops.h"
 #include "frame.h"
 #include "protocol.h"
 #include "decode/framer.h"
-
+#include "log/log.h"
 
 struct c50decoder {
 	Framer f;
 	C50RawFrame raw_frame[2];
 	C50Frame frame;
+
+	float lat, lon, alt;
+	float temp, rh;
 };
 
 #ifndef NDEBUG
@@ -43,6 +47,8 @@ c50_decoder_deinit(C50Decoder *d) {
 ParserStatus
 c50_decode(C50Decoder *self, SondeData *dst, const float *src, size_t len)
 {
+	uint32_t data;
+
 	/* Read a new frame */
 	switch(framer_read(&self->f, self->raw_frame, src, len)) {
 	case PROCEED:
@@ -61,9 +67,47 @@ c50_decode(C50Decoder *self, SondeData *dst, const float *src, size_t len)
 	}
 
 #ifndef NDEBUG
-		fwrite(&self->frame, sizeof(self->frame), 1, debug);
+	fwrite(&self->frame, sizeof(self->frame), 1, debug);
 #endif
+	log_debug_hexdump(&self->frame, sizeof(self->frame));
+	data = bitmerge(self->frame.data, 32);
 
+
+	switch (self->frame.type) {
+	case C50_TYPE_TEMP_AIR:
+		dst->fields |= DATA_PTU;
+		dst->temp = self->temp = ieee754_be(self->frame.data);
+		dst->rh = self->rh;
+		break;
+	case C50_TYPE_RH:
+		dst->fields |= DATA_PTU;
+		dst->rh = self->rh = ieee754_be(self->frame.data);
+		dst->temp = self->temp;
+		break;
+	case C50_TYPE_DATE:
+		break;
+	case C50_TYPE_TIME:
+		break;
+	case C50_TYPE_LAT:
+		dst->fields |= DATA_POS;
+		dst->lat = self->lat = (int32_t)data / 1e7 + ((int32_t)data % 10000000 / 60.0 * 100.0) / 1e7;
+		dst->lon = self->lon;
+		dst->alt = self->alt;
+		break;
+	case C50_TYPE_LON:
+		dst->fields |= DATA_POS;
+		dst->lon = self->lon = (int32_t)data / 1e7 + ((int32_t)data % 10000000 / 60.0 * 100.0) / 1e7;
+		dst->lat = self->lat;
+		dst->alt = self->alt;
+		break;
+	case C50_TYPE_ALT:
+		dst->fields |= DATA_POS;
+		dst->alt = self->alt = data / 10.0f;
+		dst->lat = self->lat;
+		dst->lon = self->lon;
+		break;
+
+	}
 
 	return PARSED;
 }
